a1d0da9382529a88e78b2584e46fb51e75388405
sjwilczynski <sjwilczynski@gmail.com>
2019-09-06 19:12:20 +0200

    basic conf for azure

---
+++ xkom-hot-shot/__init__.py
@@ -0,0 +1,59 @@
+import azure.functions as func
+
+from html import unescape
+
+import bs4
+import requests
+import os
+from price_parser import Price
+from twilio.rest import Client
+
+NEW_PRICE_CLASS = "new-price"
+OLD_PRICE_CLASS = "old-price"
+PRODUCT_NAME_ATTRIBUTE = "data-product-name"
+HOT_SHOT_ID = "#hotShot"
+XKOM_URL = "https://www.x-kom.pl"
+QUANTITY_LEFT_SOLD_CLASS = "gs-quantity"
+
+
+def main(mytimer: func.TimerRequest) -> None:
+
+    response = requests.get(XKOM_URL)
+    response.raise_for_status()
+    response_text = bs4.BeautifulSoup(response.text, features="lxml")
+
+    hot_shot_subtree = response_text.select(HOT_SHOT_ID)[0]
+
+    product_name = unescape(hot_shot_subtree.find(
+        attrs={("%s" % PRODUCT_NAME_ATTRIBUTE): True})[PRODUCT_NAME_ATTRIBUTE])
+    original_price = Price.fromstring(
+        hot_shot_subtree.find(class_=OLD_PRICE_CLASS).text)
+    new_price = Price.fromstring(
+        hot_shot_subtree.find(class_=NEW_PRICE_CLASS).text)
+    percentage_discount = str(
+        int((1 - new_price.amount / original_price.amount) * 100)) + "%"
+    [items_left, items_sold] = list(
+        map(lambda x: x.text, hot_shot_subtree.find_all(class_=QUANTITY_LEFT_SOLD_CLASS)))
+
+    left_items_part = 'By now %s were sold and %s are left.' % (
+        items_sold, items_left) if items_left is not None else 'Sold out :('
+
+    message_text = 'New deal is available: %s. The original price is %s and the new price is %s. %s discount! ' % (
+        product_name,
+        original_price.amount_text + original_price.currency,
+        new_price.amount_text + original_price.currency,
+        percentage_discount) + left_items_part
+
+    account_sid = os.environ['account_sid']
+    auth_token = os.environ['auth_token']
+    to_number = os.environ['to_number']
+    from_number = os.environ['from_number']
+
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        body=message_text,
+        from_=from_number,
+        to=to_number
+    )
+
+    print(message_text)
